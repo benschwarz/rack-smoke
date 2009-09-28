a1434cc45d46fec62b5c87a4aa1c5c231a5fc2c1
Ben Schwarz <ben.schwarz@gmail.com>
2009-09-28 21:10:26 +1000

    Added specs to validate that formats are rendered correctly

--- spec/smoke-rack_spec.rb
+++ spec/smoke-rack_spec.rb
@@ -1,7 +1,35 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "SmokeRack" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  include Rack::Test::Methods
+
+  def app
+    Smoke::Rack
+  end
+    
+  it "renders the index" do
+    get '/smoke'
+    last_response.should be_ok
+  end
+  
+  it "should list the sources" do
+    get '/smoke'
+    last_response.body.should include "<a href=\"/smoke/github\">github</a>"
+  end
+  
+  describe "github source" do
+    it "should be okay" do
+      get '/smoke/github'
+      last_response.should be_ok
+    end
+    
+    describe "formats" do
+      %w(json yaml xml).each do |format|
+        it "should be okay serving #{format}" do
+          get "/smoke/github.#{format}"
+          last_response.should be_ok
+        end
+      end
+    end
   end
 end
