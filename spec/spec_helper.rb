3afff877141d726466288f003f8f1abcb9846238
Ben Schwarz <ben.schwarz@gmail.com>
2009-09-28 20:47:41 +1000

    Load the spec environment the way we need it

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,16 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'smoke-rack'
+require 'rubygems'
 require 'spec'
 require 'spec/autorun'
+require 'rack/test'
+
+gem 'smoke', '>= 0.5.13'
+require 'smoke'
+require 'support/source'
+require 'smoke-rack'
+
+set :environment, :test
 
 Spec::Runner.configure do |config|
   
