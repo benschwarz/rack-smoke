ae5c1272a134041de931957a52c333f0657b934f
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-24 16:06:20 +1100

    Find the sources available on a per request basis

--- lib/rack/smoke.rb
+++ lib/rack/smoke.rb
@@ -17,20 +17,25 @@ module Rack
       haml :index
     end
     
-    ::Smoke.exposed_sources.keys.each do |source|
-      get "/smoke/#{source.to_s}" do
-        @source = source
-        haml :usage
-      end
+    get "/smoke/:source" do 
+      raise Sinatra::NotFound if ::Smoke[params["source"].to_sym].nil?
+      @source = ::Smoke[params["source"].to_sym]
+      haml :usage
+    end
+    
+    get "/smoke/:source/:format" do
+      raise Sinatra::NotFound if ::Smoke[params["source"].to_sym].nil?
+
+      format = params.delete("format").to_sym
+      content_type format rescue status 400
       
-      get "/smoke/#{source.to_s}.:format" do
-        format = params[:format].to_sym
-        output = ::Smoke.send(source).output(format)
-        
-        content_type format
+      begin
+        name = params.delete("source").to_sym
+        output = ::Smoke.__send__(name, params).output(format)
         etag Digest::MD5.hexdigest(output)
-        
         output
+      rescue ::Smoke::Origin::UnavailableFormat, NameError  # Requirements not met for source
+        status 400
       end
     end
   end
